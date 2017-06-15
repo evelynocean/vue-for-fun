1f62852c82b7f20109b8c87666fd575040adad87
Evelyn <evelynocean1031@gmail.com>
2017-06-15 18:38:56 +0800

    component and Axios練習

---
+++ src/components/SideNav.vue
@@ -0,0 +1,37 @@
+<template>
+    <div>
+        選單
+        <hr>
+        <select>
+            <option v-for="option in options">
+            {{ option.name }}
+            </option>
+        </select>
+        </div>
+</template>
+<script>
+import axios from 'axios'
+    
+export default {
+  data: () => ({
+    options: [
+      { id: 1, name: 'a' },
+      { id: 2, name: 'b' },
+      { id: 3, name: 'c' },
+      { id: 4, name: 'd' },
+      { id: 5, name: 'e' }
+    ]
+  }),
+  mounted () {
+      console.log(this.options, this.$options);
+      axios.get('http://evelyn.bonnie/getMenuLists')
+      .then(function (response) {
+        console.log(response.data);
+        this.options = response.data;
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+    }
+  }
+</script>
--- src/main.js
+++ src/main.js
@@ -1,9 +1,10 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+
 import Vue from 'vue'
 import App from './App'
+import SideNav from './components/SideNav'
 import router from './router'
 
+Vue.component('side-nav', SideNav)
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
