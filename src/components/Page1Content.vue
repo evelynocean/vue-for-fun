fbb58f8c68ab9eb6d55e8434e405ec4882910670
Evelyn <evelynocean1031@gmail.com>
2017-06-18 23:01:51 +0800

    表單內容練習 1

---
+++ src/components/Page1Content.vue
@@ -0,0 +1,97 @@
+<template>
+  <form class="page-1-content">
+        <div class="--con">
+            <label>title:{{title}}</label> 
+        </div>
+        <div class="--con">
+            <label>input, v-model, computed, methods</label>
+            <div>
+                <input v-model="firstName" placeholder="firstName">
+                <input v-model="lastName" placeholder="LastName">
+                {{fullName}}
+            </div>
+            <div>
+                <textarea v-model="textareaMessage" placeholder="textarea testing"></textarea>
+                <p style="white-space: pre">reversedMessage : {{ reversedMessage() }}</p>
+            </div>
+        </div>
+        <div class="--con">
+            <label>checkbox /radio (in_array, * :class test!)</label>
+            <div class='--chk'>
+                <label><input type="checkbox" value="checkbox-1" v-model="CHK" v-on:change="updateCHK" :checked="checked" :class="[checked ? 'checked-style' : '']">
+                checkbox-1</label>
+                <label><input type="checkbox" value="checkbox-2" v-model="CHK" v-on:change="updateCHK" :checked="checked" :class="[checked ? 'checked-style' : '']">
+                checkbox-2</label>
+                <label><input type="checkbox" value="checkbox-3" v-model="CHK" v-on:change="updateCHK" :checked="checked" :class="[checked ? 'checked-style' : '']">
+                checkbox-3</label>
+                {{CHK}}
+            </div>
+        </div>
+        <div class="--con">
+            <label></label>
+        </div>
+        <div class="--con">
+            <label></label>
+        </div>
+        <div class="--con">
+            <label></label>
+        </div>
+        <div class="--con">
+            <label></label>
+        </div>
+  </form>
+</template>
+<style>
+    .page-1-content .--con, .--con div {
+        padding: 12px;
+    }
+    .page-1-content label {
+        color: navy;
+        line-height: 30px;
+        font-weight: 700;
+        font-size: 1.3rem;
+        padding-left: 12px;
+    }
+
+    .--chk label {
+        color: ＃000;
+        line-height: 20px;
+        font-weight: 500;
+    }
+
+    .checked-style {
+        color: darkred;
+    }
+</style>
+<script>
+export default {
+  name: 'page1',
+  data () {
+    return {
+      title: 'page 1 content',
+      firstName: '',
+      lastName: '',
+      textareaMessage: '',
+      CHK: []
+    }
+  },
+  computed: {
+    fullName: function () {
+        console.log(this.firstName, this.lastName)
+      return this.firstName + ' ' + this.lastName
+    }
+  },
+  methods: {
+    reversedMessage: function () {
+      return this.textareaMessage.split('').reverse().join('')
+    },
+    updateCHK: function ($event) {
+        if (this.CHK.indexOf($event.target.value) >= 0) {
+           console.log('in_array');
+        } else {
+            console.log('no');
+        }
+    }
+  }
+}
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import MainContent from '@/components/MainContent'
 import Hello from '@/components/Hello'
+import Page1Content from '@/components/Page1Content'
 
 Vue.use(Router)
 
@@ -10,7 +11,7 @@ export default new Router({
         path: '/',
         component: MainContent,
         children: [
-            { path: '/name_1', component: Hello },
+            { path: '/name_1', component: Page1Content },
             { path: '/name_3', component: Hello },
             { path: '/name_5', component: Hello }
         ]
