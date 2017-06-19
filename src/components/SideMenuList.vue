2bc012681b63a28228867f9f0ee20e580c2c1773
Evelyn <evelynocean1031@gmail.com>
2017-06-19 09:39:24 +0800

    fix demo page

--- src/components/SideMenuList.vue
+++ src/components/SideMenuList.vue
@@ -56,11 +56,11 @@ import axios from 'axios'
 export default {
   data: () => ({
     items: [
-      { id: 1, name: 'tab_nav_1' },
-      { id: 2, name: 'tab_nav_2' },
-      { id: 3, name: 'tab_nav_3' },
-      { id: 4, name: 'tab_nav_4' },
-      { id: 5, name: 'tab_nav_5' }
+      { id: 1, name: 'name_1' },
+      { id: 2, name: 'name_2' },
+      { id: 3, name: 'name_3' },
+      { id: 4, name: 'name_4' },
+      { id: 5, name: 'name_5' }
     ]
   }),
   mounted () {
